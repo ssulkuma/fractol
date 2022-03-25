3e8c7bd72c643d38f25afa2e357d740e1e2e1e7a
ssulkuma <95386916+ssulkuma@users.noreply.github.com>
2022-03-25 16:29:05 +0200

    cleaner version of mandelbrot added

--- mandelbrot.c
+++ mandelbrot.c
@@ -1,81 +1,68 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mandelbrot.c                                       :+:      :+:    :+:   */
+/*   c_mandelbrot.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ssulkuma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ssulkuma <ssulkuma@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/21 15:56:27 by ssulkuma          #+#    #+#             */
-/*   Updated: 2022/03/24 15:40:34 by ssulkuma         ###   ########.fr       */
+/*   Created: 2022/03/25 15:31:28 by ssulkuma          #+#    #+#             */
+/*   Updated: 2022/03/25 16:10:13 by ssulkuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-static int	define_mandelbrot(t_mandel *mandel)
+static void	mandelbrot(t_mandel *mandel, t_complex z, t_complex c, t_mlx *mlx)
 {
-	double	temp;
-	int		iteration;
+	int	x;
+	int	y;
+	int	iteration;
 
+	x = mandel->x;
+	y = mandel->y;
 	iteration = 0;
-	mandel->z_real = 0;
-	mandel->z_imaginary = 0;
-	while ((iteration < mandel->max_iteration) && (mandel->z_real
-			* mandel->z_real + mandel->z_imaginary * mandel->z_imaginary < 4))
+	while (iteration < mandel->max_iteration && complex_abs(z) < 4)
 	{
-		temp = mandel->z_real * mandel->z_real - mandel->z_imaginary
-			* mandel->z_imaginary + mandel->c_real;
-		mandel->z_imaginary = 2 * mandel->z_real * mandel->z_imaginary
-			+ mandel->c_imaginary;
-		mandel->z_real = temp;
+		z = complex_add(complex_mul(z, z), c);
 		iteration++;
 	}
-	return (iteration);
-}
-
-static void	complex_number_mandel(t_mandel *mandel, int x, int y)
-{
-	double	real_range;
-	double	imaginary_range;
-
-	real_range = (mandel->max_real - mandel->min_real) / WIDTH;
-	imaginary_range = (mandel->max_imaginary - mandel->min_imaginary) / HEIGHT;
-	mandel->c_real = x * real_range + mandel->min_real;
-	mandel->c_imaginary = y * imaginary_range + mandel->min_imaginary;
+	if (iteration == mandel->max_iteration)
+		draw_pixel_to_image(mlx, x, y, 0x000000);
+	else
+		draw_pixel_to_image(mlx, x, y, 500 * iteration / 50);
 }
 
 static void	mandel_struct_intel(t_mandel *mandel)
 {
-	mandel->max_iteration = 50;
+	mandel->max_iteration = 100;
 	mandel->max_real = 1;
-	mandel->max_imaginary = 1;
+	mandel->max_imag = 1;
 	mandel->min_real = -2;
-	mandel->min_imaginary = -1;
+	mandel->min_imag = -1;
 }
 
 void	mandelbrot_set(t_mlx *mlx)
 {
-	int			x;
-	int			y;
-	int			iteration;
+	t_complex	z;
+	t_complex	c;
 	t_mandel	mandel;
 
 	mandel_struct_intel(&mandel);
-	mlx->color = 0xFF00FF;
-	x = 0;
-	while (x < WIDTH)
+	mandel.x = 0;
+	while (mandel.x < WIDTH)
 	{
-		y = 0;
-		while (y < HEIGHT)
+		mandel.y = 0;
+		while (mandel.y < HEIGHT)
 		{
-			complex_number_mandel(&mandel, x, y);
-			iteration = define_mandelbrot(&mandel);
-			if (iteration == mandel.max_iteration)
-				draw_pixel_to_image(mlx, x, y, 0x000000);
-			else
-				draw_pixel_to_image(mlx, x, y, 500 * iteration / 50);
-			y++;
+			z.real = 0;
+			z.imag = 0;
+			c.real = (mandel.max_real - mandel.min_real) / WIDTH * mandel.x
+				+ mandel.min_real;
+			c.imag = (mandel.max_imag - mandel.min_imag) / HEIGHT * mandel.y
+				+ mandel.min_imag;
+			mandelbrot(&mandel, z, c, mlx);
+			mandel.y++;
 		}
-		x++;
+		mandel.x++;
 	}
 }
